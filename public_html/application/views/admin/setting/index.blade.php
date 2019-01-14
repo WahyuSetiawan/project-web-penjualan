975a3bb63f8818d165485c1ca4cdecdcf512fb0f
wahyu setiawan <wahyu.creator911@gmail.com>
2019-01-15 03:29:18 +0700

    check

--- public_html/application/views/admin/setting/index.blade.php
+++ public_html/application/views/admin/setting/index.blade.php
@@ -16,7 +16,7 @@
           </div>
           <div class="col-12 col-md-9">
             <input type="text" id="nama_website" name="setting[nama_website]" placeholder="Nama Website" class="form-control"
-              value="{{(isset($setting['nama_website']))? $setting['nama_website']: ""}}">
+              value="{{(isset($setting['nama_website']))? $setting['nama_website']->value: ""}}">
             <small class="form-text text-muted">Nama Website</small>
           </div>
         </div>
@@ -26,7 +26,7 @@
             <label for="text-input" class=" form-control-label">Facebook</label>
           </div>
           <div class="col-12 col-md-9">
-            <input type="text" id="facebook" name="setting[facebook]" placeholder="Facebook" class="form-control" value="{{(isset($setting['facebook']))? $setting['facebook']: ""}}">
+            <input type="text" id="facebook" name="setting[facebook]" placeholder="Facebook" class="form-control" value="{{(isset($setting['facebook']))? $setting['facebook']->value: ""}}">
             <small class="form-text text-muted">Facebook toko</small>
           </div>
         </div>
@@ -38,7 +38,7 @@
           </div>
           <div class="col-12 col-md-9">
             <input type="text" id="instagram" name="setting[instagram]" placeholder="Instagram" class="form-control"
-              value="{{(isset($setting['instagram']))? $setting['instagram']: ""}}">
+              value="{{(isset($setting['instagram']))? $setting['instagram']->value: ""}}">
             <small class="form-text text-muted">Instagram toko</small>
           </div>
         </div>
@@ -50,7 +50,7 @@
           </div>
           <div class="col-12 col-md-9">
             <input type="text" id="phone" name="setting[phone]" placeholder="No Telepon" class="form-control"
-              value="{{(isset($setting['phone']))? $setting['phone']: ""}}">
+              value="{{(isset($setting['phone']))? $setting['phone']->value: ""}}">
             <small class="form-text text-muted">No Telepon yang dapat dihubungi</small>
           </div>
         </div>
@@ -62,7 +62,7 @@
           </div>
           <div class="col-12 col-md-9">
             <input type="text" id="email" name="setting[email]" placeholder="Email" class="form-control"
-              value="{{(isset($setting['email']))? $setting['email']: ""}}">
+              value="{{(isset($setting['email']))? $setting['email']->value: ""}}">
             <small class="form-text text-muted">Email yang dapat dihubungi</small>
           </div>
         </div>
@@ -73,7 +73,7 @@
           </div>
           <div class="col-12 col-md-9">
             <input type="text" id="alamat" name="setting[alamat]" placeholder="Alamat" class="form-control" 
-            value="{{(isset($setting['alamat']))? $setting['alamat']: ""}}">
+            value="{{(isset($setting['alamat']))? $setting['alamat']->value: ""}}">
             <small class="form-text text-muted">Alamat dari toko sekarang</small>
           </div>
         </div>
@@ -84,7 +84,7 @@
             <label for="text-input" class=" form-control-label">About Us</label>
           </div>
           <div class="col-12 col-md-9">
-            <textarea id="about_us" name="setting[about_us]" placeholder="Tentang Kami" class="form-control" >{{(isset($setting['about_us']))? $setting['about_us']: ""}}</textarea>
+            <textarea id="about_us" name="setting[about_us]" placeholder="Tentang Kami" class="form-control" >{{(isset($setting['about_us']))? $setting['about_us']->value: ""}}</textarea>
             <small class="form-text text-muted">Beberapa keterangan tentang toko</small>
           </div>
         </div>
